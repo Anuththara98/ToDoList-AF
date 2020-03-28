c6c268c984a09fee5d1dea90024534b5ea5422c2
anuththara kavindi <anukavindi@gmail.com>
2020-03-28 22:41:50 +0530

    completed without bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import TaskInput from "./Components/taskInput";
+import TodoList from "./Components/todoList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+
+    //Initialize the state and create an object
+    state = {
+        items: [],
+        id: Date.now(),
+        item: '',
+        editItem: false
+    };
+
+    //this method is tracking values which are input in the insert box.
+    changeEvent = event => {
+        this.setState({
+            item: event.target.value
+        });
+    };
+
+    //this method is listening event to the input values
+    submitTask = event => {
+        event.preventDefault();   //keep the value in the text box
+
+        const newTask = {
+            id: this.state.id,
+            title: this.state.item  //creating a new item
+        };
+
+        const updatedItems = [...this.state.items,newTask];
+
+        this.setState({
+            items: updatedItems,
+            item: "",
+            id: Date.now(),
+            editItem :false
+        });
+    };
+    clearList = () =>{
+        this.setState({
+            items:[]
+        })
+    };
+
+    deleteTask = (id) =>{
+        const filteredItems = this.state.items.filter(item =>
+        item.id !== id);
+        this.setState({
+            items: filteredItems,
+        });
+    };
+
+    updateTask = (id) => {
+        const filteredItems = this.state.items.filter(item =>
+            item.id !== id);
+        const selectedItem = this.state.items.find(item => item.id === id);
+
+        console.log(selectedItem);
+        this.setState({
+            items: filteredItems,
+            item: selectedItem.title,
+            editItem: true,
+            id:id
+        });
+    }
+        render()
+        {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <div className="col-10 mx-auto col-md-8 mt-4">
+                            <h3 className="text-capitalize text-center">To Do List</h3>
+                            <TaskInput item={this.state.item}
+                                       handleChange={this.changeEvent}
+                                       handleSubmit={this.submitTask}
+                                       editItem={this.state.editItem}/>
+                            <TodoList items={this.state.items}
+                                      clearList={this.clearList}
+                                      handleDelete={this.deleteTask}
+                                      handleEdit={this.updateTask}/>
+                        </div>
+                    </div>
+                </div>
+
+            )
+        }
 }
 
 export default App;
